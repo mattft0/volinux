4a83bbb0109ba8123e9f4ca93742b9b1ab927de1
mattft0 <matteofloirat112440@gmail.com>
2025-03-22 17:30:41 +0100

    Cleaning the docker system before starting the container

--- start.sh
+++ start.sh
@@ -17,5 +17,9 @@ fi
 
 echo "Détection du noyau : ${KERNEL_VERSION}"
 
+# Nettoyage du système Docker
+echo "Nettoyage du système Docker..."
+docker system prune -f
+
 # Lancement de Docker Compose avec les arguments
 docker-compose build --no-cache --build-arg FILE_NAME="${FILE_NAME}" --build-arg KERNEL_VERSION="${KERNEL_VERSION}"
